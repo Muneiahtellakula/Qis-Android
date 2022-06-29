9d6d26c338816ec234ec265c83192b69e9e0d262
muneiah <muneiahtellakula@gmail.com>
2022-06-29 16:12:44 +0530

    Retrofit

---
+++ Covid19Reports/app/src/main/java/com/example/covid19reports/Covid19Instance.java
@@ -0,0 +1,4 @@
+package com.example.covid19reports;
+public class Covid19Instance {
+    
+}
---
+++ Covid19Reports/app/src/main/java/com/example/covid19reports/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.covid19reports;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.covid19reports.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+    /*Retrofit
+    * It is one of Lib for fetching the backgrond opatations
+    **/
+}
