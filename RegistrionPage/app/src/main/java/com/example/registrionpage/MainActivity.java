5267bd9359ad17e271f9d8861d2767e2f1513ed0
muneiah <muneiahtellakula@gmail.com>
2022-06-29 12:53:29 +0530

    Input Controls app added

--- RegistrionPage/app/src/main/java/com/example/registrionpage/MainActivity.java
+++ RegistrionPage/app/src/main/java/com/example/registrionpage/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.registrionpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.registrionpage.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+       binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+binding.submitButton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        String name=binding.etUserName.getText().toString();
+        String email=binding.etEmail.getText().toString();
+        Toast.makeText(getApplicationContext(),
+                "Name :"+name+
+                "\n Email:"+email, Toast.LENGTH_SHORT).show();
+    }
+});
     }
 }
